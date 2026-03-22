538af2ced125886f06909f0b919a333f11a83b56
Fulgence N'da <iatennis88@gmail;com>
2026-03-22 21:18:33 +0100

    Fix cle dupliquee ignore bouton

--- modules/prediction.py
+++ modules/prediction.py
@@ -545,7 +545,7 @@ def page_prediction(modeles, df_base):
                             st.session_state["joueur_b_auto"] = match["joueur_b"]
                             st.session_state["ignorer_api"] = True
                     with col_no:
-                        if st.button("❌ Ignorer", key=f"ignore_{match['joueur_a']}"):
+                        if st.button("❌ Ignorer", key=f"ignore_{match['joueur_a']}_{match['joueur_b']}_a1"):
                             st.session_state["ignorer_api"] = True
                 st.markdown("---")
 
